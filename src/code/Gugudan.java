59d785d8dade9b128838f94e5fc6328315238a5d
mac <mac@macui-MacBookAir.local>
2020-05-02 21:08:09 +0900

    initial program for Gugudan_Program

---
+++ src/code/Gugudan.java
@@ -0,0 +1,29 @@
+package code;
+
+import java.util.Scanner;
+
+public class Gugudan {
+
+	public static void main(String args[])
+	{
+		// any number times table
+		System.out.print("What number for times table? : ");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int number = scan.nextInt();
+		
+		System.out.println(number + " * 1 = " + number * 1);
+		System.out.println(number + " * 2 = " + number * 2);
+		System.out.println(number + " * 3 = " + number * 3);
+		System.out.println(number + " * 4 = " + number * 4);
+		System.out.println(number + " * 5 = " + number * 5);
+		System.out.println(number + " * 6 = " + number * 6);
+		System.out.println(number + " * 7 = " + number * 7);
+		System.out.println(number + " * 8 = " + number * 8);
+		System.out.println(number + " * 9 = " + number * 9);
+		
+		
+	}
+	
+}
