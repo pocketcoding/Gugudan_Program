af6ce3074fffd589faaac6fdf1095b8e03cfb4a4
mac <mac@192.168.nate.com>
2020-05-03 13:41:19 +0900

    for/while loop statment -> 6 & 7 times table

--- src/code/Gugudan.java
+++ src/code/Gugudan.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Gugudan {
 
-	public static void main(String args[])
+	public static void main(String[] args)
 	{
 		// any number times table
 		System.out.print("What number for times table? : ");
@@ -22,6 +22,28 @@ public class Gugudan {
 		System.out.println(number + " * 7 = " + number * 7);
 		System.out.println(number + " * 8 = " + number * 8);
 		System.out.println(number + " * 9 = " + number * 9);
+		
+		System.out.println("---------------------");
+		
+		// 'while' looping statement -> making 6 times table
+		
+		int number6 = 6;
+		int i = 1;
+		while(i <= 9)
+		{ 
+			System.out.println(i + " x " + number6+ " = " + i*number6);
+			i++;
+		}
+		
+		System.out.println("---------------------");
+		
+		// 'for' looping statement -> making 7 times table
+		
+		int number7 = 7;
+		for(i = 1; i <= 9; i++)
+		{
+			System.out.println(i + " x " + number7+ " = " + i*number7);
+		}
 		
 		
 	}
