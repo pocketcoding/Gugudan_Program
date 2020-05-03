f8b6a47d4f9d5f1e1fc2c8c1e9b70bf407ffc9d6
mac <mac@192.168.nate.com>
2020-05-03 14:40:12 +0900

    if (conditional statement) -> printing 2~9 times table

--- src/code/Gugudan.java
+++ src/code/Gugudan.java
@@ -23,9 +23,9 @@ public class Gugudan {
 		System.out.println(number + " * 8 = " + number * 8);
 		System.out.println(number + " * 9 = " + number * 9);
 		
-		System.out.println("---------------------");
+		System.out.println("----------- End of step 1 ----------"); // divider
 		
-		// 'while' looping statement -> making 6 times table
+		// 'while' looping statement -> printing 6 times table
 		
 		int number6 = 6;
 		int i = 1;
@@ -35,15 +35,40 @@ public class Gugudan {
 			i++;
 		}
 		
-		System.out.println("---------------------");
-		
-		// 'for' looping statement -> making 7 times table
+		// 'for' looping statement -> printing 7 times table
 		
 		int number7 = 7;
 		for(i = 1; i <= 9; i++)
 		{
 			System.out.println(i + " x " + number7+ " = " + i*number7);
 		}
+		
+		System.out.println("----------- End of step 2 ----------"); // divider
+		
+		// 'if' conditional statement -> printing any number times table
+		
+		System.out.print("What number for times table? : ");
+		scan = new Scanner(System.in);
+		int user_input = scan.nextInt();
+		System.out.println("Your number : " + user_input);
+		
+		if(user_input < 2)
+		{
+			System.out.println("Error : Choose any number between 2 ~ 9");
+		}
+		else if (user_input > 9)
+		{
+			System.out.println("Error : Choose any number between 2 ~ 9");
+		} else
+		{
+			for(int j = 1; j <= 9; j++)
+			{
+				System.out.println(j + " x " + user_input + " = " + j*user_input);
+				i++;
+			}
+		}
+		
+		System.out.println("----------- End of step 3 ----------"); // divider
 		
 		
 	}
